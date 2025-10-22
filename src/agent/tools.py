d86e28352ddaefdd327ab011c1993809f03599d5
Waqas Ahmed <waqasahmed75@gmail.com>
2025-10-22 22:49:25 +0500

    weather api

--- src/agent/tools.py
+++ src/agent/tools.py
@@ -1,9 +1,19 @@
 from agents.tool import function_tool
+import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+weather_api_key = os.getenv("WEATHER_API_KEY")
 
 @function_tool
-def get_weather(location: str, unit: str = "C") -> str:
+def get_weather(location: str) -> str:
   """
   Fetch the weather for a given location, returning a short description.
   """
+  response = requests.get(
+      f"http://api.weatherapi.com/v1/current.json?key={weather_api_key}&q={location}"
+  )
+  data = response.json()
   # Example logic
-  return f"The weather in {location} is 22 degrees {unit}."
+  return f"The current weather in {location} is {data['current']['temp_c']}C degree with {data['current']['condition']['text']}."
